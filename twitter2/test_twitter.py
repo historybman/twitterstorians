e1457e98a22ab0f88570908f1b05b7f9bebe3c0a
Bob Morrissey <rmorriss@illinois.edu>
2017-03-20 22:37:14 -0500

    removed OAuth

--- twitter2/test_twitter.py
+++ twitter2/test_twitter.py
@@ -10,9 +10,8 @@ import json
 # consumer_key = input("enter your consumer_key: ")
 # consumer_secret = input("enter your consumer_secret: ")
 
-# t = Twitter(auth = OAuth(token, token_secret, consumer_key, consumer_secret))
+t = Twitter(auth = OAuth(token, token_secret, consumer_key, consumer_secret))
 
-t = Twitter(auth = OAuth("437803182-oC6F7n34TKPNgJ0hkEDSJ94ZDt1O2dcjKuYqbjpu", "vVagdZXd9S7czx6m5r4E4qmWiyreSMLRSIfnDnXv2ziUg", "gYaQqLlPplMjAuDWk5krCjV4W", "SbmqAT4XiVbvJ3pbDlkG15s4G9HzcRK2RVcDbXdoqi4ieDwO1X"))
 
 info = t.search.tweets(q="#twitterstorians")
 
